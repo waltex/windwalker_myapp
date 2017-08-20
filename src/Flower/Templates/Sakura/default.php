e203e603d257c9a5ef7fbab8a2717201ad61a230
Walter Cardelli (imac) <waltex@libero.it>
2017-08-20 23:33:53 +0200

    test windwalker

--- src/Flower/Templates/Sakura/default.php
+++ src/Flower/Templates/Sakura/default.php
@@ -1,15 +1,56 @@
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 <?php
+
 // src/Flower/Templates/sakura/default.php
-/**
- * @var  $this  \Windwalker\Core\Renderer\PhpRenderer
- */
+
+use Windwalker\Form\Field\TextareaField;
+use Windwalker\Form\Form;
+use Windwalker\Form\Field\TextField;
+use Windwalker\Form\Field\PasswordField;
+?>
+<h1>Hello <?php echo $this->escape($name); ?></h1>
+
+
+<?php
+$form = new Form;
+
+$form->addField(new TextField('username', 'Username'));
+$form->addField(new PasswordField('password', 'Password'));
+$form->addField(new TextField('email', 'Email'));
+$form->addField(new TextareaField('description', 'Description'));
+
+echo $form->renderFields();
 ?>
 
-<?php $this->extend('_global.html') ?>
 
-<!-- This will override parent `content` block -->
-<?php $this->block('content'); ?>
-<div class="container">
-    <h1>Hello <?php echo $this->escape($name); ?></h1>
+<div class="input-group">
+  <span class="input-group-addon" id="basic-addon1">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
 </div>
-<?php $this->endblock(); ?>
+
+<div class="input-group">
+  <input type="text" class="form-control" placeholder="Recipient's username" aria-describedby="basic-addon2">
+  <span class="input-group-addon" id="basic-addon2">@example.com</span>
+</div>
+
+<div class="input-group">
+  <span class="input-group-addon">$</span>
+  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+  <span class="input-group-addon">.00</span>
+</div>
+
+<label for="basic-url">Your vanity URL</label>
+<div class="input-group">
+  <span class="input-group-addon" id="basic-addon3">https://example.com/users/</span>
+  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
